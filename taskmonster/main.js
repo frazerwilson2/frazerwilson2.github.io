7ce86dce21f93ca4feba3e1dcc30e972644c5f0d
frazer wilson <frazerwilson145@gmail.com>
2017-07-15 21:31:13 +0100

    v1

--- taskmonster/main.js
+++ taskmonster/main.js
@@ -1,3 +1,6 @@
+//var colors = ['#16a085', '#2ecc71', '#2980b9', '#9b59b6', '#34495e', '#e67e22', '#f39c12', '#d35400', '#4DD0E1', '#FFD54F', '#7986CB'];
+var colors = ['extra', 'minny', 'spike', 'lou', 'other', 'thingy', 'magic', 'crystal', 'arou', 'zen', 'panda'];
+var colorLen = colors.length;
 
 function setPeps(){
 $('#toDoHolder .pep').pep({
@@ -20,65 +23,48 @@ $('#toDoHolder .pep').pep({
 };
 
 setPeps();
+var toDoRecords = [];
 
 function myFunction(){
-      var inputVal = $('#toDoItem').val();
-    if(!inputVal) {return}
-    var newId = Math.round(Math.random() * 10000000000);
-  var newToDoObj = {id: newId, name:inputVal, type:1, left:$(window).width() / 2, top: $(window).height() / 2};
-  toDoRecords.push(newToDoObj);
+  var inputVal = $('#toDoItem').val();
+  if(!inputVal) {return}
+  var newId = Math.round(Math.random() * 10000000000);
+  var chosenClr = colors[Math.round(Math.random() * colorLen)];
+  var newLeft = $(window).width() / 2 + randomPos();
+  var newTop = $(window).height() / 2 + randomPos();
+  var newToDoObj = {id: newId, name:inputVal, type:1, left: newLeft, top: newTop, colour: chosenClr};
+  if(!toDoRecords){
+  	toDoRecords = [{newToDoObj}]
+  }
+  else {
+  	toDoRecords.push(newToDoObj);
+  }
   localStorage.setItem('dragToDoRecs', JSON.stringify(toDoRecords));
   buildToDoMonster(newToDoObj);
     setPeps();
   $('#toDoItem').val("");
 };
- var toDoRecords = [];
 
 $(document).ready(function(){
   toDoRecords = JSON.parse(localStorage.getItem('dragToDoRecs'));
   //alert(toDoRecords);
-  toDoRecords.forEach(function(item){
-    buildToDoMonster(item);
-  });
-  setPeps();  
+  if(toDoRecords){
+	  toDoRecords.forEach(function(item){
+	    buildToDoMonster(item);
+	  });
+	  setPeps();  	
+	  $('#toDoItem').focus();
+  }
 });
 
-// function myFunction() {
-// //    alert("The form was submitted");
-//     var inputVal = $('#toDoItem').val();
-//   alert(inputVal);
-//     if(!inputVal) {return}
-//     var newId = Math.round(Math.random() * 10000000000);
-//   var newToDoObj = {id: newId, name:inputVal, type:1};
-//   toDoRecords.push(newToDoObj);
-//   localStorage.setItem('dragToDoRecs', JSON.stringify(toDoRecords));
-//   buildToDoMonster(inputVal, newId);
-//     setPeps();
-//   $('#toDoItem').val("");
-// }
-
-// $('button').addEventListener('touchmove', function(e) {
-// //$('button').touchstart(function(){
-//   alert('x');
-// //  e.preventDefault();
-//   var inputVal = $('#toDoItem').val();
-//   alert(inputVal);
-//   if(!inputVal) {return}
-//   var newId = Math.round(Math.random() * 10000000000);
-//   var newToDoObj = {id: newId, name:inputVal, type:1};
-//   toDoRecords.push(newToDoObj);
-//   localStorage.setItem('dragToDoRecs', JSON.stringify(toDoRecords));
-//   buildToDoMonster(inputVal, newId);
-//   setPeps();
-//   $('#toDoItem').val("");
-// });
-  
-// });
+$('input').on('keyup', function(e) {
+	if ($("input").is(":focus") && (e.keyCode == 13)) {
+		myFunction();
+    }
+})
 
 function setToDoPos(pos, id){
-  console.log(pos);
     for(var i=0;i<toDoRecords.length;i++){
-    console.log(toDoRecords[i]);
     if(toDoRecords[i].id == id){
       toDoRecords[i].left = pos.left;
       toDoRecords[i].top = pos.top;
@@ -89,7 +75,6 @@ function setToDoPos(pos, id){
 
 function removeToDo(id){
   for(var i=0;i<toDoRecords.length;i++){
-    console.log(toDoRecords[i]);
     if(toDoRecords[i].id == id){
       toDoRecords.splice(i, 1);
     }
@@ -97,11 +82,16 @@ function removeToDo(id){
   localStorage.setItem('dragToDoRecs', JSON.stringify(toDoRecords));
 }
 
+function randomPos() {
+	return Math.round(Math.random() * 200) - Math.round(Math.random() * 200);
+}
+
 function buildToDoMonster(item){
   var newToDo = $( "#template .pep" ).clone();
   newToDo.children('.name').html(item.name);
   $(newToDo).attr('id', item.id);
+$(newToDo).find('.mbody').addClass(item.colour);
   newToDo.appendTo($('#toDoHolder'));
+
 newToDo.css({top: item.top, left: item.left});
-  //  newToDo.css({top: (document.body.innerHeight / 2) - 50, left: (document.body.innerWidth / 2) + 25});
 }
