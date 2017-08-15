1e0c0f6ee2505b781132e4b355674bff5317fb8a
Frazer Wilson <frazer.wilson@dm-companies.com>
2017-08-15 09:55:18 +0100

    Update main.js

--- taskmonster/main.js
+++ taskmonster/main.js
@@ -117,6 +117,6 @@ function toggleViews() {
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/sw.js')
+        .register('sw.js')
         .then(function() { console.log('Service Worker Registered'); });
 }
